36c6cc334910119d8eb0ff6b4f8f4ee61927e108
Adrian Orłów <adrian@orlow.me>
2025-11-21 02:24:08 +0100

    lint: fix

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,5 @@
-import react from "@vitejs/plugin-react";
 import path from "node:path";
+import react from "@vitejs/plugin-react";
 import { defineConfig } from "vitest/config";
 
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
